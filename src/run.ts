bd93da43169c74b4aca23d9f5068fd7a14e69908
Paweł Otto <git@avelo.tech>
2018-11-19 09:48:52 +0100

    Adds config file

--- src/run.ts
+++ src/run.ts
@@ -1,19 +1,27 @@
 import * as config from 'config'
-import { Connection } from 'amqplib'
 import connectRabbitMQ from './functions/connect-rabbitmq'
 import rabbitMQQueueWriter from './functions/rabbitmq-queue-writer'
+import { Connection } from 'amqplib'
 
-main()
+main();
 
 async function main() {
-  const rabbitMQSourceString = config.get("rabbitMQSourceString") as string
-  const rabbitMQTargetString = config.get("rabbitMQTargetString") as string
-  const rabbitMQSourceConnection: Connection | undefined = await connectRabbitMQ(rabbitMQSourceString)
-  const rabbitMQTargetConnection: Connection | undefined = await connectRabbitMQ(rabbitMQTargetString)
-  const rabbitMQQueue: string = config.get('rabbitMQQueue')
+  const rabbitMQSourceString = config.get("rabbitMQSourceString") as string;
+  const rabbitMQTargetString = config.get("rabbitMQTargetString") as string;
+  const rabbitMQSourceConnection:
+    | Connection
+    | undefined = await connectRabbitMQ(rabbitMQSourceString);
+  const rabbitMQTargetConnection:
+    | Connection
+    | undefined = await connectRabbitMQ(rabbitMQTargetString);
+  const rabbitMQQueue: string = config.get("rabbitMQQueue");
   if (rabbitMQSourceConnection && rabbitMQTargetConnection) {
-    rabbitMQQueueWriter(rabbitMQSourceConnection, rabbitMQTargetConnection, rabbitMQQueue)
+    rabbitMQQueueWriter(
+      rabbitMQSourceConnection,
+      rabbitMQTargetConnection,
+      rabbitMQQueue
+    );
   } else {
-    console.error('No RabbitMQ connection. Giving up.')
+    console.error("No RabbitMQ connection. Giving up.");
   }
 }
